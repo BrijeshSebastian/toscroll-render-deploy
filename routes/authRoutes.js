56a0db296084672a0a55e9525ea2935d546c1723
“Brijesh <brijeshseb11@gmail.com.com>
2025-06-17 11:56:01 +0530

    Remove multer

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,22 +8,6 @@ const upload = require('../Cloudinary/cloudinaryStorage');
 
 const router = express.Router();
 
-const multer = require('multer');
-const path = require('path');
-
-// Set storage config
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/profile_photos');
-  },
-  filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname);
-    cb(null, `${Date.now()}-${file.fieldname}${ext}`);
-  }
-});
-
-const upload = multer({ storage: storage });
-
 
 // Register
 router.post('/register', async (req, res) => {
